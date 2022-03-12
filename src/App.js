459ca4e11ca6164ad19a5105ea5d281827a733f6
teruteru3733 <98194935+teruteru3733@users.noreply.github.com>
2022-03-12 17:05:05 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {InlineStyle} from "./components/InlineStyle";
+import {CssModules} from "./components/CssModules";
+
+export default function App() {
+
+  return (
+    <div className="App">
+
+      <InlineStyle />
+      <CssModules />
+    </div>
+  );
+}
+
